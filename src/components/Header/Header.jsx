4fe5ff6c094e4b7483bfc65a99e17756bb89c0fa
AxelCortes162 <axelcorjmaxd@gmail.com>
2026-04-27 23:26:11 -0600

    logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header-container">
-      <img src="/logolargo.png" alt="Logo" className="header-logo" />
+      <img src="/logolargo.svg" alt="Logo" className="header-logo" />
       {/* <h1 className="header-title">Botanas Mony</h1> */}
       <p className="header-subtitle">¡Antójate y pide tus favoritas!</p>
     </header>
